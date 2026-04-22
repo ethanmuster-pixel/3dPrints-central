c6d082ae18f32db2a231cc833606a26d93213672
ethanmuster-pixel <ethan.m.uster@gmail.com>
2026-04-22 14:46:24 -0700

    Add files via upload

--- stock.js
+++ stock.js
@@ -1,12 +1,12 @@
 var stock = {
-  "iPhone 13": true,
-  "iPhone 14": true,
-  "iPhone 15": true,
-  "iPhone 16": true,
-  "iPhone 17": true,
-  "Simple Color Case": true,
-  "Fidget Cube": true,
-  "Infinity Flipper Ring": true,
-  "Gear Spinner": true,
-  "Butterfly Knife": true
+  "iPhone 13": 5,
+  "iPhone 14": 5,
+  "iPhone 15": 5,
+  "iPhone 16": 5,
+  "iPhone 17": 5,
+  "Simple Color Case": 5,
+  "Fidget Cube": 5,
+  "Infinity Flipper Ring": 5,
+  "Gear Spinner": 5,
+  "Butterfly Knife": 5
 };
