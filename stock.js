89d995503459aca9d038e3913755f8227e0bd99d
ethanmuster-pixel <ethan.m.uster@gmail.com>
2026-04-22 14:48:53 -0700

    Update stock.js

--- stock.js
+++ stock.js
@@ -1,12 +1,12 @@
 var stock = {
-  "iPhone 13": 5,
-  "iPhone 14": 5,
-  "iPhone 15": 5,
-  "iPhone 16": 5,
-  "iPhone 17": 5,
-  "Simple Color Case": 5,
-  "Fidget Cube": 5,
-  "Infinity Flipper Ring": 5,
-  "Gear Spinner": 5,
-  "Butterfly Knife": 5
+  "iPhone 13": 1,
+  "iPhone 14": 1,
+  "iPhone 15": 1,
+  "iPhone 16": 1,
+  "iPhone 17": 1,
+  "Simple Color Case": 1,
+  "Fidget Cube": 1,
+  "Infinity Flipper Ring": 0,
+  "Gear Spinner": 0,
+  "Butterfly Knife": 2,
 };
