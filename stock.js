00d5aba584fa22c20aa0cb2353978c28b0556784
ethanmuster-pixel <ethan.m.uster@gmail.com>
2026-04-27 14:42:37 -0700

    Add files via upload

--- stock.js
+++ stock.js
@@ -9,4 +9,27 @@ var stock = {
   "Infinity Flipper Ring": 1,
   "Gear Spinner": 0,
   "Butterfly Knife": 1,
+  "Spiral Top": 5,
+  "Helix Twist": 5,
+  "Stretch Egg": 5,
+  "Fidget Bundle": 5
+};
+
+// Add item names here to show a NEW badge
+var newItems = [
+  "Spiral Top",
+  "Helix Twist",
+  "Stretch Egg",
+  "Fidget Bundle"
+];
+
+// Add item discounts here (percentage off, e.g. 20 = 20% off)
+var saleItems = {
+  // "Gear Spinner": 20
+};
+
+// Discount codes (code: percentage off)
+var discountCodes = {
+  "SAVE10": 10,
+  "BUNDLE5": 5
 };
